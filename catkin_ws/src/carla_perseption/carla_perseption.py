09e3d9c1c3470c447d800e3e653bce244aac03fd
Roman Kvasnyj <kvasnyj@gmail.com>
2018-01-17 13:26:47 +0100

    Add message type

--- catkin_ws/src/carla_perseption/carla_perseption.py
+++ catkin_ws/src/carla_perseption/carla_perseption.py
@@ -1,18 +1,23 @@
 import rospy
 
-import random
+#import random
 import sys
 import time
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
+sys.path.insert(0, '/carla')
 from carla.client import make_carla_client
 from carla.sensor import Camera
 from carla.settings import CarlaSettings
 from carla.tcp import TCPConnectionError
 from carla.util import print_over_same_line
 from Line import Line
+
+from geometry_msgs.msg import PoseStamped, TwistStamped
+from std_msgs.msg import float32
+from styx_msgs.msg import CarState
 
 class CarlaPerseption(object):
     def __init__(self):
@@ -21,7 +26,7 @@ class CarlaPerseption(object):
         rospy.logdebug("CarlaPerseption started")
         rospy.init_node('carla_perseption', log_level=rospy.DEBUG)
 
-        self.measurements_pub = rospy.Publisher('measurements', Measurements, queue_size=1)
+        self.carstate_pub = rospy.Publisher('carstate', CarState, queue_size=1)
 
         rospy.spin()
 
@@ -29,6 +34,8 @@ class CarlaPerseption(object):
         while True:
             try:
                 #run_carla_client('localhost', 2000)
+
+                #self.carstate_pub.publish(carstate)
 
                 print('Done.')
                 return
@@ -47,6 +54,6 @@ if __name__ == '__main__':
         left_lane = Line()
         right_lane = Line()
 
-        main()        
+        main()
     except rospy.ROSInterruptException:
-        rospy.logerr('Could not start waypoint updater node.')        
+        rospy.logerr('Could not start waypoint updater node.')
