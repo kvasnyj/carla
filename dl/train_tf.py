9852d72c9a23bed0851d34d5a96dbdf0d8f3ec0e
kvasnyj <kvasnyj@gmail.com>
2018-03-05 23:43:23 +0100

    checkpoint

--- dl/train_tf.py
+++ dl/train_tf.py
@@ -1,7 +1,17 @@
 import tensorflow as tf
+from tensorflow.contrib.layers import flatten
 import numpy as np
 import glob
 from sklearn.model_selection import train_test_split
+import cv2
+import math
+
+n_classes = 2
+std = 0.1
+EPOCHS = 20
+BATCH_SIZE = 100
+rate = 0.0001
+
 
 def image_pipeline(file):
     img = cv2.imread(file)
@@ -10,6 +20,12 @@ def image_pipeline(file):
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img =  img[:,:, np.newaxis]
     return img
+
+def array2classifier(M, array):
+    N = len(array)
+    resultArray = np.zeros((N, M), float)
+    resultArray[np.arange(N), array] = 1.
+    return resultArray    
 
 def get_data():
     X = []
@@ -23,11 +39,22 @@ def get_data():
         X.append(image_pipeline(file))
         y.append(0)
 
-    y = np_utils.to_categorical(y, 2)
-
     return train_test_split(np.array(X), np.array(y), test_size=0.2)
 
-def get_model_nvidia(): #source of the model: http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+def split2batches(batch_size, features, labels):
+    assert len(features) == len(labels)
+    outout_batches = []
+
+    sample_size = len(features)
+    for start_i in range(0, sample_size, batch_size):
+        end_i = start_i + batch_size
+
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+        outout_batches.append(batch)
+
+    return outout_batches    
+
+def get_model_lenet(x): #source of the model: http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
     # 28x28x6
     x = tf.reshape(x, (-1, 32, 32, 1))
     conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), stddev=std))
@@ -57,11 +84,18 @@ def get_model_nvidia(): #source of the model: http://images.nvidia.com/content/t
     fc1_W = tf.Variable(tf.truncated_normal(shape=(fc1_shape), stddev=std))
     fc1_b = tf.Variable(tf.zeros(120))
     fc1 = tf.matmul(fc1, fc1_W) + fc1_b
+    #fc1 = tf.nn.dropout(fc1, 0.5)
     fc1 = tf.nn.relu(fc1)
 
     fc2_W = tf.Variable(tf.truncated_normal(shape=(120, n_classes), stddev=std))
     fc2_b = tf.Variable(tf.zeros(n_classes))
     return tf.matmul(fc1, fc2_W) + fc2_b
+
+def eval_data(X_valid, y_valid):
+    num_examples = len(X_valid)
+    loss, acc = sess.run([loss_op, accuracy_op], feed_dict={x: batch_x, y: batch_y})
+
+    return loss, acc
 
 X_train, X_test, y_train, y_test =  get_data()
 n_train = len(X_train)
@@ -70,4 +104,46 @@ n = n_train + n_test
 
 print("size: %s, train: %s, test: %s" % (n, n_train, n_test))
 
-model = get_model_nvidia()
+image_shape = X_train[0].shape
+x = tf.placeholder(tf.float32, (None, image_shape[0], image_shape[1], 1))
+y = tf.placeholder(tf.float32, (None, n_classes))
+learning_rate = tf.placeholder(tf.float32, shape=[])
+
+model = get_model_lenet(x)
+
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = model, labels = y))
+correct_prediction = tf.equal(tf.argmax(model, 1), tf.argmax(y, 1))
+accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = opt.minimize(loss_op)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(EPOCHS):
+    X_epoch, X_valid, y_epoch, y_valid = train_test_split(X_train, y_train, test_size=0.2)
+    batches = split2batches(BATCH_SIZE, X_epoch, y_epoch)
+
+    steps_per_epoch = len(X_epoch) // BATCH_SIZE
+
+    for step in range(steps_per_epoch):
+        batch = batches[step]
+        batch_x = batch[0]
+        batch_y = array2classifier(n_classes, batch[1])
+        loss = sess.run(train_op, feed_dict={x: batch_x, y: batch_y, learning_rate: rate})
+
+    val_loss, val_acc = eval_data(X_valid, y_valid)
+    print("EPOCH {} ...".format(i+1))
+    print("Validation loss = {:.3f}".format(val_loss))
+    print("Validation accuracy = {:.3f}".format(val_acc))
+
+    print("rate", rate)
+    #rate = max(rate*0.5, 0.00001)
+
+    print()
+        
+# Evaluate on the test data
+test_loss, test_acc = eval_data(X_test, array2classifier(n_classes, y_test))
+print("Test loss = {:.3f}".format(test_loss))
+print("Test accuracy = {:.3f}".format(test_acc))        
